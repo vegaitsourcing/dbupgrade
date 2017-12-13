f8f8d7b33a7b9d1fa35e2cbed6d960229bd28787
Nemanja Malocic <n.malocic@emakina.nl>
2017-12-13 10:08:48 +0100

    Changed mySqlDatabase to use Database abstract class, that way ConnectionString is read from app config automatically. Added test to check if connection string is populated during object creation

---
+++ Vega.DbUpgrade.Tests/TestMySqlDatabase.cs
@@ -0,0 +1,15 @@
+﻿using Vega.DbUpgrade.Databases;
+using Xunit;
+
+namespace Vega.DbUpgrade.Tests
+{
+    public class TestMySqlDatabase
+    {
+        [Fact]
+        public void MySqlDatabase_ReadConnectionString()
+        {
+            MySqlDatabase database = new MySqlDatabase();
+            Assert.NotNull(database.DbConnectionString);
+        }
+    }
+}
--- Vega.DbUpgrade/Databases/MySqlDatabase.cs
+++ Vega.DbUpgrade/Databases/MySqlDatabase.cs
@@ -1,29 +1,26 @@
 ﻿using System.Data;
 using MySql.Data.MySqlClient;
-using Vega.DbUpgrade.Interfaces;
 
 namespace Vega.DbUpgrade.Databases
 {
     /// <summary>
     /// Concrete implementation for MySQL database.
     /// </summary>
-    public class MySqlDatabase : IDatabase
+    public class MySqlDatabase : Database
     {
-        public string DbConnectionString { get; set; }
-
         #region [Public Methods]
         
-        public IDbConnection GetDbConnection()
+        public override IDbConnection GetDbConnection()
         {
             return new MySqlConnection(DbConnectionString);
         }
 
-        public IDbCommand GetDbCommand()
+        public override IDbCommand GetDbCommand()
         {
             return new MySqlCommand();
         }
 
-        public IDbCommand GetDbCommand(string commandText, IDbConnection connection)
+        public override IDbCommand GetDbCommand(string commandText, IDbConnection connection)
         {
             return new MySqlCommand(commandText, (MySqlConnection)connection);
         }
